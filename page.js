467e6525d53407b7f78443e1d6e5d6c8b7dd418a
Dominic Thomas <dominic.thomas@grapplemobile.com>
2012-11-26 16:38:40 +0000

    comments added to page.js with some iplayer calls

--- page.js
+++ page.js
@@ -1,1 +1,12 @@
 alert("I'm in!");
+/*
+
+Once the player is playing you can call:
+embeddedMedia.playerInstances.emp.call("play") or
+embeddedMedia.playerInstances.emp.call("pause") 
+
+TODO: Find out what is set when player is initially started (some weird custom event shit)
+
+Note: iplayer object and related modules are defined in episode.js
+
+*/
